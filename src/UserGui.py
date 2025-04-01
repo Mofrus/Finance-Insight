e353529cd580630b215f1249a710956a4df590a7
Mofrus <vojtula5@email.cz>
2025-04-01 17:03:26 +0200

    Fix: one line fix

--- src/UserGui.py
+++ src/UserGui.py
@@ -31,10 +31,10 @@ def show_day_low_callback():
     with dpg.group(parent="library_window"):
         dpg.add_input_text(label="Stock", tag="stock_input")
         dpg.add_radio_button(items=["High", "Low"], horizontal=True)
-        dpg.add_button(label="Print", callback=lambda: print(StockDataHandler.get_high_or_low(StockDataHandler.search_stock(dpg.get_value("stock_input")), period="1d", high=dpg.get_value("High"))))
+        dpg.add_button(label="Print", callback=lambda: print(StockDataHandler.get_high_or_low(StockDataHandler
+                                                             .search_stock(dpg.get_value("stock_input")), period="1d", high=dpg.get_value("High"))))
 
 dpg.create_context()
-
 with dpg.window(label="Navbar", tag="navbar", width=150, height=600, no_move=True, no_title_bar=True):
     dpg.add_button(label="Compare Stocks", callback=compare_stocks_callback)
     dpg.add_button(label="Show Day Low", callback=show_day_low_callback)
